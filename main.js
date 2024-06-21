841eea85dd4e53b3c66096f9e0a93fa4087c53a7
Noah <nkillianruckman@ivytech.edu>
2024-06-20 23:21:48 -0400

    Major Changes... Reworked CSS to mobile first Added Media Queries for 768 and 1024.

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const slideshowImages = document.querySelectorAll(".slideshow-container .slideshow-img");
-
-const nextImageDelay = 4500;
-let currentImageCounter = 0;
-
-slideshowImages[currentImageCounter].style.opacity = 1;
-
-setInterval(nextImage, nextImageDelay);
-
-function nextImage() {
-    slideshowImages[currentImageCounter].style.opacity = 0;
-    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+document.addEventListener("DOMContentLoaded", () => {
+    const slideshowImages = document.querySelectorAll(".slideshow-container .slideshow-img");
+    
+    const nextImageDelay = 4500;
+    let currentImageCounter = 0;
+    
     slideshowImages[currentImageCounter].style.opacity = 1;
-}
+    
+    setInterval(nextImage, nextImageDelay);
+    
+    function nextImage() {
+        slideshowImages[currentImageCounter].style.opacity = 0;
+        currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+        slideshowImages[currentImageCounter].style.opacity = 1;
+    }
+});
