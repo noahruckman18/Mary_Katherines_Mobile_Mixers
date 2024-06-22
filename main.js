e31c263f53c72b6b709ff3dd1e46e3fe13e221a8
Noah <nkillianruckman@ivytech.edu>
2024-06-22 16:55:37 -0400

    Updated Form to make it clear and redirect to custome page.

--- main.js
+++ main.js
@@ -21,3 +21,43 @@ function submitForm() {
     frm.reset();  // Reset all form data
     return True; // Prevent page refresh
  }  
+
+
+ const form = document.getElementById('form');
+ const result = document.getElementById('result');
+ 
+ form.addEventListener('submit', function(e) {
+   e.preventDefault();
+   const formData = new FormData(contact-form);
+   const object = Object.fromEntries(formData);
+   const json = JSON.stringify(object);
+   result.innerHTML = "Please wait..."
+ 
+     fetch('https://noahruckman18.github.io/Mary_Katherines_Mobile_Mixers/Submit.html', {
+             method: 'POST',
+             headers: {
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json'
+             },
+             body: json
+         })
+         .then(async (response) => {
+             let json = await response.json();
+             if (response.status == 200) {
+                 result.innerHTML = "Form submitted successfully";
+             } else {
+                 console.log(response);
+                 result.innerHTML = json.message;
+             }
+         })
+         .catch(error => {
+             console.log(error);
+             result.innerHTML = "Something went wrong!";
+         })
+         .then(function() {
+             form.reset();
+             setTimeout(() => {
+                 result.style.display = "none";
+             }, 3000);
+         });
+ });
